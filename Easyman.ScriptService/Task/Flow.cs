ef32e447ec9f008e9b318f2724295eba27debc5f
chengsen <419746405@qq.com>
2018-04-01 18:31:25 +0800

    no message

--- Easyman.ScriptService/Task/Flow.cs
+++ Easyman.ScriptService/Task/Flow.cs
@@ -45,22 +45,30 @@ namespace Easyman.ScriptService.Task
             if (scr!=null&&scr.ID>0)
             {
                 WriteLog(0, BLog.LogLevel.DEBUG, string.Format("获取当前任务组【" + ID + "】为并行任务。"));
+                try
+                {
 
-                #region 判断监控文件表中是否有待处理的monit_file. 没有将跳出任务组实例的创建
-                string sql = string.Format(@"select count(1) from FM_MONIT_FILE where COPY_STATUS=0 or COPY_STATUS=3");
-                object obj = null;
-                using (BDBHelper dbop = new BDBHelper())
-                {
-                    obj = dbop.ExecuteScalar(sql);
+                    #region 判断监控文件表中是否有待处理的monit_file. 没有将跳出任务组实例的创建
+                    string sql = string.Format(@"select count(1) from FM_MONIT_FILE where COPY_STATUS=0 or COPY_STATUS=3");
+                    object obj = null;
+                    using (BDBHelper dbop = new BDBHelper())
+                    {
+                        obj = dbop.ExecuteScalar(sql);
+                    }
+                    if (obj != null && Convert.ToInt32(obj) > 0)
+                    { }
+                    else
+                    {
+                        WriteLog(0, BLog.LogLevel.INFO, string.Format("并行任务【自动上传文件】无待上传文件，将不创建任务组实例", ID));
+                        return false;
+                    }
+                    #endregion
                 }
-                if (obj != null && Convert.ToInt32(obj) > 0)
-                { }
-                else
+                catch (Exception ex)
                 {
-                    WriteLog(0, BLog.LogLevel.INFO, string.Format("并行任务【自动上传文件】无待上传文件，将不创建任务组实例", ID));
+                    WriteLog(0, BLog.LogLevel.INFO, string.Format("监控错误：{0}", ex.Message));
                     return false;
                 }
-                #endregion
 
                 lock (this)
                 {
